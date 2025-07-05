4fcde798a502eae3b3552d7bc98258c0bb3eb649
Ashok Gunji <21ne5a0225@gmail.com>
2025-07-05 21:44:08 +0530

    addesd version8 project

---
+++ section8/cards/src/main/java/com/tekworks/cards/exception/CardAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.tekworks.cards.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class CardAlreadyExistsException extends RuntimeException {
+
+    public CardAlreadyExistsException(String message){
+        super(message);
+    }
+
+}
